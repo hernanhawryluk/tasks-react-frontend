477e49e21b466db9a4386249c3d5cf533d9b998c
Hernan Hawryluk <hernanhawryluk@gmail.com>
2023-12-30 14:58:29 -0300

    Adjusted layout to improve overall user experience

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,13 +5,6 @@ const API_URL = "https://tasks-nodejs-backend.vercel.app/api";
 const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    "Access-Control-Allow-Credentials": true,
-  },
 });
 
 export default instance;
