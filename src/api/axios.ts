003ff9c6894961cf3f67d2bb70ba0a7cdc6622e0
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-01-02 08:37:41 -0300

    Deploy on Vercel

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://tasks-react-frontend.vercel.app//api";
+const API_URL = "https://tasks-react-frontend.vercel.app/api";
 // const API_URL = "http://localhost:3000/api";
 
 const instance = axios.create({
