e0ce688dcbe4adb9e989d488c01244a89fe68375
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-01-02 09:27:03 -0300

    Added a CircularProgress from MUI

--- src/utils/ProtectedRoutes.tsx
+++ src/utils/ProtectedRoutes.tsx
@@ -1,10 +1,16 @@
+import { CircularProgress } from "@mui/material";
 import { useAuth } from "../context/AuthContext";
 import { Navigate, Outlet } from "react-router-dom";
 
 function ProtectedRoutes() {
   const { loading, isAuthenticated } = useAuth();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <div className="flex justify-center items-center">
+        <CircularProgress size={80} />
+      </div>
+    );
   if (!loading && !isAuthenticated) return <Navigate to="login" />;
 
   return <Outlet />;
