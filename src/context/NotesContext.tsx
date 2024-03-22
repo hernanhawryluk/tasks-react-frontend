8ababfe8fbd53424ce43a19469dcffcb125a6c4b
Hernan Hawryluk <hernanhawryluk@gmail.com>
2024-03-22 12:24:47 -0300

    Fixed a bug with notes

--- src/context/NotesContext.tsx
+++ src/context/NotesContext.tsx
@@ -31,7 +31,7 @@ export function NotesProvider({ children }: NotesProviderProps) {
   const getNotes = async () => {
     try {
       const res = await getNotesRequest();
-      if (res.data.notes) setNotes(res.data.notes);
+      if (res.data?.notes) setNotes(res.data.notes);
       else setNotes("");
     } catch (error) {
       console.log(error);
